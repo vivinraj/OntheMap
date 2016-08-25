989a69b1cb6e5a730e0865020ec90ca43b8811a9
vivin raj <VR@vivins-macbook-pro.local>
2016-08-25 19:23:18 +0530

    Issue with login

--- OntheMaps/LoginViewController.swift
+++ OntheMaps/LoginViewController.swift
@@ -54,11 +54,84 @@ class LoginViewController: UIViewController, UITextFieldDelegate, UIAlertViewDel
     func loginPressed() {
         let username: String? = Username.text
         let password = Password.text
-        
         print("Username: \(username!)")
         print("Password: \(password!)")
-        
+    
         let request = NSMutableURLRequest(URL: NSURL(string: Constants.udacityBaseURL + "/session")!)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = "{\"udacity\": {\"username\": \"\(username!)\", \"password\": \"\(password!)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            /*guard(error == nil) else {
+                print("Inside error: \(error)")
+                return
+            }
+            guard let data = data else {
+                print("No data")
+                return
+            }*/
+            let newData = data!.subdataWithRange(NSMakeRange(5, data!.length - 5)) // subset response data!
+            print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+            
+            /*let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(newData, options: .AllowFragments)
+                //print("parsedResult: \(parsedResult)")
+            }
+            catch {
+                print("error")
+                return
+            }
+            //print("parsedResult: \(parsedResult)")
+            //print("Parsed result error: \(parsedResult["error"])")
+            
+            guard let account = parsedResult["session"] as? NSDictionary else {
+                return
+            }
+            //print("Account: \(account)")
+            self.appDelegate.sessionID = account["id"] as? String
+            //print("SessionID: \(self.appDelegate.sessionID)")
+            guard let session = parsedResult["account"] as? NSDictionary else {
+                return
+            }
+            //print("Parsed result for key: \(session)")
+            self.appDelegate.keyID = session["key"] as? String //Not sure if this is the correct key id
+            self.appDelegate.userID = username
+            
+            self.getUserData()
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                let nc = self.storyboard!.instantiateViewControllerWithIdentifier("mapTabViewController") as! UITabBarController
+                self.presentViewController(nc, animated: true, completion: nil)
+            });*/
+        }
+        
+        
+        task.resume()
+
+ 
+ 
+        /*let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = "{\"udacity\": {\"username\": \"vimalrajmohan@gmail.com\", \"password\": \"C0ldhoraws\"}}".dataUsingEncoding(NSUTF8StringEncoding)*/
+        //let session = NSURLSession.sharedSession()
+        //let task = session.dataTaskWithRequest(request) { data, response, error in
+          /*  if error != nil { // Handle error…
+                return
+            }
+            let newData = data!.subdataWithRange(NSMakeRange(5, data!.length - 5)) // subset response data! /
+                print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+            
+           
+        }
+        task.resume()*/
+        
+        /*let request = NSMutableURLRequest(URL: NSURL(string: Constants.udacityBaseURL + "/session")!)
         request.HTTPMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -152,7 +225,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate, UIAlertViewDel
                 });
         }
         }
-        task.resume()
+        task.resume() */
         
     }
     
@@ -168,27 +241,20 @@ class LoginViewController: UIViewController, UITextFieldDelegate, UIAlertViewDel
                 return
             } else {
                 guard let data = data else {
-                    return
+                return
                 }
-                
                 let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5))
+                
                 let parsedResult = try! NSJSONSerialization.JSONObjectWithData(newData, options: .AllowFragments) as! NSDictionary
-                
                 print("Parsed Result in getUserData: \(parsedResult)")
-                guard let user = parsedResult["user"] as? NSDictionary else {
-                    return
-                }
                 
-               /* guard let error = parsedResult["error"] as? NSDictionary else {
-                    return
-                }*/
+                guard let user = parsedResult["user"] as? NSDictionary else {
+                return
+                }
                 
                 self.appDelegate.fName = user["first_name"] as? String
                 self.appDelegate.lName = user["last_name"] as? String
                 print("First name : \(self.appDelegate.fName) AND Last Name: \(self.appDelegate.lName)")
-                
-                
-                
             }
         }
         task.resume()
